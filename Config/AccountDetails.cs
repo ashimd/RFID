3958fb8d727d22e4c2f98088fc40b50394b77ffc
Ashim <ashim.sp@gmail.com>
2017-11-15 17:23:52 +0530

    Create AccountDetails.cs

---
+++ Config/AccountDetails.cs
@@ -0,0 +1,9 @@
+namespace RFID.Common.Config
+{
+    public class AccountDetails
+    {
+        //ToDo: Enter a valid Serivce Bus connection string
+        public static string ConnectionString = "";
+        public static string QueueName = "rfidcheckout";
+    }
+}
